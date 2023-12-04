a8212c1d6e5e893ccf7b70d2d8ae6144bfdb4c71
Siddharth Golla <siddharth.golla87@gmail.com>
2023-12-04 23:08:39 +0530

    Set the bg color and created the Grid

--- Game Of Life/main.py
+++ Game Of Life/main.py
@@ -1,2 +1,48 @@
+import pygame
 from settings import *
 from sprites import *
+
+#  Main Game Class Declaration
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        pass
+
+    def draw_grid(self):
+        for row in range(0, HEIGHT, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (0, row), (WIDTH, row))
+
+        for col in range(0, WIDTH, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (col, 0), (col, HEIGHT))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        pygame.display.flip()
+
+
+    
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+
+
+# Game loop initializing
+if __name__ == '__main__':
+    game = Game()
+    game.run()
--- Game Of Life/settings.py
+++ Game Of Life/settings.py
@@ -1,5 +1,16 @@
 #--- Colors (r,g,b) ---#
 WHITE = (255,255,255)
 BLACK = (0,0,0)
-LIGHTGREY = (40,40,40)
-DARKGREY = (100,100,100)
+DARKGREY = (40,40,40)
+LIGHTGREY = (100,100,100)
+BGCOLOR = DARKGREY
+
+# --- Dimensions --- #
+TILESIZE = 15
+ROWS = COLS = 50
+WIDTH = (ROWS * TILESIZE) + 1
+HEIGHT = (COLS * TILESIZE) + 1
+
+# --- Others -- #
+FPS = 60
+TITLE = "Game Of Life"
