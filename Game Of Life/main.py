39597a0ef85bb071bd95e338c0f5ca3a469431a4
Siddharth Golla <siddharth.golla87@gmail.com>
2023-12-10 23:19:55 +0530

    GOL: Added left mouse click event to make cell alive and cell color changed

--- Game Of Life/main.py
+++ Game Of Life/main.py
@@ -4,7 +4,14 @@ from sprites import *
 
 #  Main Game Class Declaration
 class Game:
+    """
+    Game class represents the main game loop and handles the game's logic.
+    """
     def __init__(self) -> None:
+        """
+        Initialize the game. Set up the screen, clock, and grid.
+        """
+
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption(TITLE)
@@ -12,6 +19,9 @@ class Game:
         self.grid = Grid()
 
     def run(self):
+        """
+        Run the game. This is the main game loop.
+        """
         self.playing = True
         while self.playing:
             self.clock.tick(FPS)
@@ -24,6 +34,9 @@ class Game:
 
 
     def draw(self):
+        """
+        Draw the game state. This is where you would render the game state to the screen.
+        """
         self.screen.fill(BGCOLOR)
         self.grid.draw_grid(self.screen)
         pygame.display.flip()
@@ -31,13 +44,26 @@ class Game:
 
     
     def events(self):
+        """
+        Handle game events. This is where you would handle user input and other events.
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit(0)
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for row in self.grid.cells:
+                    for cell in row:
+                        if cell.is_clicked(mouse_x, mouse_y):
+                            cell.alive = not cell.alive
 
 
 # Game loop initializing
 if __name__ == '__main__':
+    """
+    Main entry point for the game. Creates a Game object and starts the game loop.
+    """
     game = Game()
     game.run()
--- Game Of Life/sprites.py
+++ Game Of Life/sprites.py
@@ -2,17 +2,27 @@ import pygame
 from settings import *
 
 class Cell:
+    """
+    Cell class represents a single cell in the grid.
+    """
     def __init__(self, x, y):
+        """
+        Initialize a cell at the given x and y coordinates.
+        """
         self.x = x
         self.y = y
         self.alive = False
 
 
-    # Draw the cell if it is alive
     def draw(self, screen):
+        """
+        Draw the cell on the given screen if it is alive.
+        """
         if self.alive:
-            pygame.draw.rect(screen, WHITE, (self.x, self.y, TILESIZE, TILESIZE))
+            pygame.draw.rect(screen, CELL_COLOR, (self.x, self.y, TILESIZE, TILESIZE))
 
+    def is_clicked(self, mouse_x, mouse_y):
+        return self.x <= mouse_x < (self.x + TILESIZE) and self.y <= mouse_y < (self.y + TILESIZE)
 
     def __str__(self):
         return f"({self.x},{self.y})"
@@ -21,12 +31,21 @@ class Cell:
 
 
 class Grid:
+    """
+    Grid class represents the game grid.
+    """
     def __init__(self):
+        """
+        Initialize the grid, cells list and add cell objects.
+        """
         self.cells = []
         self.create_cells_list()
         
     
     def create_cells_list(self):
+        """
+        Create a list of cells for the grid.
+        """
         for x in range(0, WIDTH, TILESIZE):
             cells_row = []
             for y in range(0, HEIGHT, TILESIZE):
@@ -35,13 +54,20 @@ class Grid:
 
 
     def draw_grid(self, screen):
+        """
+        Draw the grid on the given screen.
+        """
+
+        # Draw the cells
         for row in self.cells:
             for cell in row:
                 cell.draw(screen)
 
 
+       # Draw vertical lines
         for row in range(0, HEIGHT, TILESIZE):
             pygame.draw.line(screen, LIGHTGREY, (0, row), (WIDTH, row))
 
+       # Draw horizontal lines
         for col in range(0, WIDTH, TILESIZE):
             pygame.draw.line(screen, LIGHTGREY, (col, 0), (col, HEIGHT))
